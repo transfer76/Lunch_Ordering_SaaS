7e3336a22a5c02ff6389548065386c2d776dd132
transfer76 <shina76@ukr.net>
2019-08-21 15:05:44 +0300

    Add strong params to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,16 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, exept: [:index, :show]
+  before_action :set_current_user, exept: [:index, :show]
 
   def index
     @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
-  def new
-    @user = User.new
-  end
-
-  def edit
-  end
-
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      redirect_to @user, notice: 'User was successfully created.'
-    else
-      render :new
-    end
-  end
+  def edit; end
 
   def update
     if @user.update(user_params)
@@ -42,11 +28,11 @@ class UsersController < ApplicationController
 
   private
 
-  def set_user
-    @user = User.find(params[:id])
-  end
+  def set_current_user
+      @user = current_user
+    end
 
   def user_params
-    params.fetch(:user, {})
+    params.fetch(:user, {}).permit(:username, :email, :password)
   end
 end
