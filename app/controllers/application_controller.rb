8c42d83c2b05cb1414c458447a2d5cb41380acde
transfer76 <shina76@ukr.net>
2019-08-21 13:57:33 +0300

    Add user edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  after_action :verify_authorized, except: :index, unless: :devise_controller?
+  #after_action :verify_authorized, except: :index, unless: :devise_controller?
   #after_action :verify_policy_scoped, only: :index
 
   def configure_permitted_parameters
